eb840e959f72eafac3eb6b985c76d4bfbd3843fd
kathirmeyyappan <kathirmey@gmail.com>
2023-08-31 11:18:32 -0400

    changing tests to make bigger files

--- file_compression_demo/decompressed_test_files/decompressed_c_test.c
+++
@@ -1,3 +0,0 @@
-// I don't even know C lol
-
-;lsijkdfngdijknsgpfsdngpihjbsd 9sf uy9gfiuygas doyfasuifafd 09gasd fygoasdfyg
--- file_compression_demo/test_files/javascript_test.js
+++
@@ -1,5 +0,0 @@
-// this is a js file to show that we can compress any text-based file!
-
-for (let i = 0 ; i < 100; i++) {
-    print(i)
-}
