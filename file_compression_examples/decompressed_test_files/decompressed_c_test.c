1b13f11fcb93cf0af1a74980b4d14ac7383e02d3
kathirmeyyappan <kathirmey@gmail.com>
2023-09-02 16:46:36 -0400

    more tests

---
+++ file_compression_examples/decompressed_test_files/decompressed_c_test.c
@@ -0,0 +1,25 @@
+// this is to show that compression can work with any file as long as they're text-based!
+
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
+for (int i = 0; psyche oin even know c lol);
